2b3a910748f1487cddb76e97992eb96c7f3e94fa
codedjay672 <emmajoel6456@gmail.com>
2025-04-18 18:55:00 +0100

    implement banner carousel with swiperjs

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import Carousel from "@/components/Carousel";
+
 export default function Home() {
-  return <section className="content-wrapper"></section>;
+  return (
+    <section className="content-wrapper">
+      <Carousel slides={["/assets/banner.png", "/assets/branded-banner.png"]} />
+    </section>
+  );
 }
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React, { useRef } from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+
+const Carousel = ({ slides }: { slides: string[] }) => {
+  const prevRef = useRef<HTMLDivElement | null>(null);
+  const nextRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div className="relative">
+      <Swiper
+        pagination={{ dynamicBullets: true }}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        modules={[Pagination, Navigation, Autoplay]}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        loop
+        className="w-full h-40 lg:h-60 rounded-xl"
+      >
+        {slides.map((slide, idx) => (
+          <SwiperSlide key={idx}>
+            <Image src={slide} alt="tmg" fill />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div
+        ref={prevRef}
+        className="absolute top-0 left-0 z-10 group h-full flex-center hover:bg-dark-200/20 hover:shadow-2xs transition-all"
+      >
+        <ChevronLeftIcon
+          size={44}
+          className="text-dark-100/50 group-hover:text-dark-100/75 transition-all"
+        />
+      </div>
+      <div
+        ref={nextRef}
+        className="absolute top-0 right-0 z-10 h-full flex-center hover:bg-dark-200/20 hover:shadow-2xs"
+      >
+        <ChevronRightIcon
+          size={40}
+          className="text-dark-100/50 group-hover:text-dark-100/75 transition-all"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
