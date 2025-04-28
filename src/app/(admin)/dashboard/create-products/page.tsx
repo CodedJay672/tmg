9cc1781cfa345e98489b4b210dacf002da2b9261
codedjay672 <emmajoel6456@gmail.com>
2025-04-28 21:56:10 +0100

    fix: remove modal route

--- src/app/(admin)/dashboard/create-products/page.tsx
+++ src/app/(admin)/dashboard/create-products/page.tsx
@@ -5,19 +5,20 @@ import React from "react";
 const CreateProducts = () => {
   return (
     <section className="dashboard-container flex flex-col">
-      <h2 className="text-lg lg:text-xl font-medium">Add Products</h2>
-      <p className="text-sm lg:text-base text-dark-300">
-        Please enter the products details
-      </p>
-
-      <div className="flex-1 flex-center flex-col gap-4">
+      <div className="flex-1 pt-24">
         <Image
           src="/assets/logo.png"
           alt="tmg procurement"
           width={140}
           height={32}
-          className="rounded-full"
+          className="rounded-full mx-auto"
         />
+        <h2 className="text-lg lg:text-xl text-center font-medium">
+          Add Products
+        </h2>
+        <p className="text-sm lg:text-base text-center text-dark-300">
+          Please enter the products details
+        </p>
         <ProductForm />
       </div>
     </section>
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,18 +1,11 @@
 import AdminNavLinks from "@/components/AdminNavLinks";
 import React from "react";
 
-const AdminLayout = ({
-  modal,
-  children,
-}: {
-  children: React.ReactNode;
-  modal: React.ReactNode;
-}) => {
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="w-full flex flex-col lg:flex-row gap-4">
       <AdminNavLinks />
       {children}
-      {modal}
     </main>
   );
 };
