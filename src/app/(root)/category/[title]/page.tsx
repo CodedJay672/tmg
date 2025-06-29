89398cd63adb005f311c6dee492f34bd3ba99b83
codedjay672 <emmajoel6456@gmail.com>
2025-06-29 21:23:19 +0100

    implement category revamp

--- src/app/(root)/category/[title]/page.tsx
+++ src/app/(root)/category/[title]/page.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Segments from "@/components/shared/Segments";
-import ProductGallery from "@/components/ProductGallery";
 import { getLoggedInUser } from "@/lib/server/appwrite";
+import { Loader2Icon } from "lucide-react";
+import { getAllProducts } from "@/lib/actions/products.actions";
+import { getUser } from "@/lib/actions/user.actions";
+import ProductCard from "@/components/shared/ProductCard";
 
 const Category = async ({
   params,
@@ -12,17 +15,42 @@ const Category = async ({
 }) => {
   const { title } = await params;
   const { page } = await searchParams;
+
+  //get products and users
+  const allProducts = await getAllProducts(+page, title);
   const user = await getLoggedInUser();
+  const currentUser = await getUser(user?.$id);
 
   return (
-    <section className="content-wrapper flex-center flex-col">
+    <section className="content-wrapper">
       <div className="py-6 w-full">
         <Segments title={title} />
       </div>
 
-      <div className="w-full flex-1">
-        <ProductGallery param={page} query={title} userId={user?.$id} />
-      </div>
+      <Suspense
+        fallback={
+          <div className="flex-center gap-2">
+            <Loader2Icon size={20} className="text-primary animate-spin" />
+            <span className="text-base">Fetching products</span>
+          </div>
+        }
+      >
+        {allProducts?.status ? (
+          <div className="w-full flex-1 grid grid-cols-2 lg:grid-cols-5 gap-1 lg:gap-4 mt-6">
+            {allProducts?.data?.map((product) => (
+              <ProductCard
+                key={product.$id}
+                item={product}
+                user={currentUser.data?.documents?.[0]}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-base lg:text-lg text-gray-300 text-center w-full mt-10">
+            No products in {title} category.
+          </p>
+        )}
+      </Suspense>
     </section>
   );
 };
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -31,15 +31,21 @@ export default async function Home({
           </div>
         }
       >
-        <div className="w-full flex-1 grid grid-cols-2 lg:grid-cols-5 gap-1 lg:gap-4 mt-6">
-          {allProducts?.data?.map((product) => (
-            <ProductCard
-              key={product.$id}
-              item={product}
-              user={currentUser.data?.documents?.[0]}
-            />
-          ))}
-        </div>
+        {allProducts?.status ? (
+          <div className="w-full flex-1 grid grid-cols-2 lg:grid-cols-5 gap-1 lg:gap-4 mt-6">
+            {allProducts?.data?.map((product) => (
+              <ProductCard
+                key={product.$id}
+                item={product}
+                user={currentUser.data?.documents?.[0]}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-base lg:text-lg text-gray-300 text-center w-full mt-10">
+            No products to view
+          </p>
+        )}
       </Suspense>
     </section>
   );
