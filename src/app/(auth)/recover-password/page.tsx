e965ad3eb07a08cac18574f24844e700659c2306
codedjay672 <emmajoel6456@gmail.com>
2025-07-01 11:45:36 +0100

    remove home link from auth pages

--- src/app/(auth)/recover-password/page.tsx
+++ src/app/(auth)/recover-password/page.tsx
@@ -6,13 +6,6 @@ import React from "react";
 const RecoverPassword = () => {
   return (
     <section className="w-full max-w-lg bg-foreground border border-gray-300 rounded-lg py-6 px-3 space-y-4">
-      <Link
-        href="/"
-        className="text-base text-primary font-medium place-self-end"
-      >
-        Go Home
-      </Link>
-
       <div className="w-full flex-center flex-col gap-2 my-6">
         <Image
           src="/assets/logo.png"
@@ -23,7 +16,6 @@ const RecoverPassword = () => {
         />
         <p className="text-base font-medium">Recover your password.</p>
       </div>
-
       <PasswordRecoveryForm />
     </section>
   );
--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -12,13 +12,6 @@ const ResetPassword = async ({
 
   return (
     <section className="w-full max-w-lg bg-foreground border border-gray-300 rounded-lg py-6 px-3 space-y-4">
-      <Link
-        href="/"
-        className="text-base text-primary font-medium place-self-end"
-      >
-        Go Home
-      </Link>
-
       <div className="w-full flex-center flex-col gap-2 my-6">
         <Image
           src="/assets/logo.png"
